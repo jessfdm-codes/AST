938157f8bd73eadf57cf9356d9abe6731b34b693
Thomas Fynn Douglas Moorhouse <thomas.moorhouse@sky.uk>
2020-03-29 23:00:46 +0100

    Added inital gameplay

---
+++ Assets/StrokeTarget.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrokeTarget : MonoBehaviour
+{
+
+    public TargetBoard TargetBoard {get; set;}
+    private bool mouseOver;
+    private float remaining = 100.0f;
+
+    [SerializeField]
+    private SpriteRenderer radius;
+
+    void Awake(){
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update(){
+        if (mouseOver){
+            remaining -= Time.deltaTime * 50f;
+            radius.material.SetFloat("_Cutoff", Mathf.Max((remaining / 100f), float.Epsilon));
+            Debug.Log(remaining);
+        }
+
+        if (remaining <= 0.0f){
+            TargetBoard.NotifyPointScored();
+        }
+    }
+
+
+    void OnMouseEnter(){
+        Debug.Log("hello");
+        mouseOver = true;
+    }
+
+    void OnMouseExit(){
+        mouseOver = false;
+    }
+}
---
+++ Assets/TargetBoard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBoard : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetPrefab;
+    private StrokeTarget currTarget;
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NotifyPointScored(){
+        score++;
+        Destroy(currTarget.gameObject);
+        currTarget = null;
+        SpawnTarget();
+    }
+
+    private void SpawnTarget(){
+        Vector3[] corners = new Vector3[4];
+        ((RectTransform) transform).GetWorldCorners(corners);
+
+        //Get random point
+        float x = Mathf.Lerp(corners[0].x, corners[3].x, Random.Range(0f,1f));
+        float y = Mathf.Lerp(corners[0].y, corners[1].y, Random.Range(0f,1f));
+
+        var newGo = Instantiate(targetPrefab);
+        (newGo.transform as RectTransform).position = new Vector2(x, y);
+        currTarget = newGo.GetComponent<StrokeTarget>();
+        currTarget.TargetBoard = this;
+    }
+}
