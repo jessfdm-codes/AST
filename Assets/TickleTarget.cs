ccfb9292a9ed9d5aa58eb0eef1a11aa609de0489
Sam <samjameslaw@gmail.com>
2020-03-31 20:41:35 +0100

    Added tickle class

---
+++ Assets/TickleTarget.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TickleTarget : Target
+{
+    [SerializeField]
+    private int minRepetitions = 4;
+    [SerializeField]
+    private int maxRepetitions = 6;
+    private string[] keys = { "a", "s", "d", "f" };
+    private Stack<string> nextKey = new Stack<string>();
+    private int repetitions;
+
+    void Start()
+    {
+        keys = keys.Reverse().ToArray();
+        repetitions = Random.Range(minRepetitions, maxRepetitions);
+
+        for (int i = 0; i < repetitions; i++) {
+            foreach (string key in keys) nextKey.Push(key);
+        }
+    }
+
+    private void Update()
+    {
+        switch (nextKey.Peek().ToUpper()) {
+            case "A":
+                if (Input.GetKeyDown(KeyCode.A)) IncreaseDoggyHappiness();
+                break;
+            case "S":
+                if (Input.GetKeyDown(KeyCode.S)) IncreaseDoggyHappiness();
+                break;
+            case "D":
+                if (Input.GetKeyDown(KeyCode.D)) IncreaseDoggyHappiness();
+                break;
+            case "F":
+                if (Input.GetKeyDown(KeyCode.F)) IncreaseDoggyHappiness();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void IncreaseDoggyHappiness() {
+        nextKey.Pop();
+
+        remaining -= (100f / (repetitions * keys.Length));
+        UpdateRadius();
+
+        if (remaining <= 0.0f){
+            TargetBoard.NotifyPointScored();
+        }
+    }
+}
