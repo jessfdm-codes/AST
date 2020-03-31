d2f3b9d4c1eb4d7843f55bafc5528a4583040ccc
Sam <samjameslaw@gmail.com>
2020-03-31 21:05:22 +0100

    Show which behaviour is now active

--- Assets/TargetBoard.cs
+++ Assets/TargetBoard.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TargetBoard : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] targetPrefabs;
+    [SerializeField]
+    private Text nextBehaviourText;
     private Target currTarget;
     private int score = 0;
 
@@ -36,9 +39,26 @@ public class TargetBoard : MonoBehaviour
         float x = Mathf.Lerp(corners[0].x, corners[3].x, Random.Range(0f,1f));
         float y = Mathf.Lerp(corners[0].y, corners[1].y, Random.Range(0f,1f));
 
-        var newGo = Instantiate(targetPrefabs[(int) Random.Range(0, targetPrefabs.Length)]);
+        var newGo = Instantiate(targetPrefabs[Random.Range(0, targetPrefabs.Length)]);
         (newGo.transform as RectTransform).position = new Vector2(x, y);
         currTarget = newGo.GetComponent<Target>();
         currTarget.TargetBoard = this;
+
+        switch (newGo.name) {
+            case "RubTarget(Clone)":
+                nextBehaviourText.text = "Rub!";
+                break;
+            case "SpinTarget(Clone)":
+                nextBehaviourText.text = "Spin!";
+                break;
+            case "StrokeTarget(Clone)":
+                nextBehaviourText.text = "Stroke!";
+                break;
+            case "TickleTarget(Clone)":
+                nextBehaviourText.text = "Tickle!";
+                break;
+            default:
+                break;
+        }
     }
 }
