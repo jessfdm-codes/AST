05162e42225327780dd62438f76db90e735c5027
Sam <samjameslaw@gmail.com>
2020-04-04 16:18:07 +0100

    Added the "park" scene & transition to "game" scene

---
+++ Assets/DogController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DogController : MonoBehaviour
+{
+    private bool mouseOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("noted...");
+            StartCoroutine(LoadDogStroke());
+        }
+    }
+
+    private IEnumerator LoadDogStroke() {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("game");
+        Debug.Log("loading...");
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    private void OnMouseEnter()  => mouseOver = true;
+    private void onMouseExit() => mouseOver = false;
+}
