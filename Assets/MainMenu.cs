f20e1165fb553697cc966988f069e1fbea6b69e9
Thomas Fynn Douglas Moorhouse <thomas.moorhouse@sky.uk>
2020-04-05 10:53:34 +0100

    Added Main Menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class MainMenu : MonoBehaviour
+{
+    private Animator animator;
+
+    // TRIGGERS
+    private string _T_GOTO_MAIN = "GoTo_Main";
+    private string _T_GOTO_MODESELECT = "GoTo_ModeSelect";
+    private string _T_EXIT = "Exit";
+
+    void Awake(){
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X)){
+            Button_Stroke();
+        }
+    }
+
+    public void Button_Stroke(){
+        animator.SetTrigger(_T_GOTO_MODESELECT);
+        animator.SetTrigger(_T_EXIT);
+    }
+
+    public void Button_StrokeTogether(){
+        animator.SetTrigger(_T_GOTO_MAIN);
+        animator.SetTrigger(_T_EXIT);
+    }
+
+    public void Button_StrokeAlone(){
+        animator.SetTrigger(_T_GOTO_MAIN);
+        animator.SetTrigger(_T_EXIT);
+    }
+}
